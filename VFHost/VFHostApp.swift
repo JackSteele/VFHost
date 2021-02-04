f63953e5dc1f6f041f68bae4e317420828ba4789
Jack Steele <jacks4899@gmail.com>
2021-02-04 13:33:57 -0800

    Initial Commit

---
+++ VFHost/VFHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  VFHostApp.swift
+//  VFHost
+//
+//  Created by Jack Steele on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct VFHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
