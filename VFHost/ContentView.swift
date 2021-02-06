bee657b1ff2c1dc0ccf949404abaacaaec74c179
Jack Steele <jacks4899@gmail.com>
2021-02-05 19:08:22 -0800

    initial commit

--- VFHost/ContentView.swift
+++ VFHost/ContentView.swift
@@ -6,12 +6,205 @@
 //
 
 import SwiftUI
+import Virtualization
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @ObservedObject var VM = VirtualMachine()
+        
+    @State var errorShown = false
+    @State var errorMessage = ""
+    @State var started = false
+    
+    @StateObject var vp = VMParameters()
+    
+    func loadData() {
+        if let def = UserDefaults.standard.string(forKey: "kernelPath") {
+            vp.kernelPath = def
+        }
+        if let def = UserDefaults.standard.string(forKey: "ramdiskPath") {
+            vp.ramdiskPath = def
+        }
+        if let def = UserDefaults.standard.string(forKey: "diskPath") {
+            vp.diskPath = def
+        }
     }
+    
+    var body: some View {
+        VStack {
+            Text(started ? "VM running" : "VM stopped").font(.largeTitle)
+            
+            Form {
+                Text("Kernel path")
+                HStack {
+                    TextField("~/distribution/vmlinuz", text: $vp.kernelPath)
+                    Button("Select file") {
+                        vp.kernelPath = openFile(kind: "kernel")
+                    }
+                }
+                
+                Text("Ramdisk path")
+                HStack {
+                    TextField("~/distribution/initrd", text: $vp.ramdiskPath)
+                    Button("Select file") {
+                        vp.ramdiskPath = openFile(kind: "ramdisk")
+                    }
+                }
+                
+                Text("Disk image path")
+                HStack {
+                    TextField("~/distribution/disk.img", text: $vp.diskPath)
+                    Button("Select file") {
+                        vp.diskPath = openFile(kind: "disk image")
+                    }
+                }
+                
+                Text("Kernel parameters")
+                HStack {
+                    TextField("console=hvc0", text: $vp.kernelParams)
+                }
+            }
+            .disabled(started)
+            .padding()
+            
+            Form {
+                HStack {
+                    Text("CPU cores allocated")
+                    Toggle(isOn: $vp.autoCore, label: {
+                        Text("Auto")
+                    })
+                    Spacer()
+                    Text(vp.autoCore ? "" : "\(Int(vp.coreAlloc)) core(s)")
+                    
+                }
+                
+                Slider(value: $vp.coreAlloc,
+                       in: VM.minCore...VM.maxCore,
+                       step: 1
+                )
+                .padding(.horizontal, 10)
+                .disabled(vp.autoCore)
+                
+                HStack {
+                    Text("Memory allocated")
+                    Toggle(isOn: $vp.autoMem, label: {
+                        Text("Auto")
+                    })
+                    Spacer()
+                    Text(vp.autoMem ? "" : String(format: "%.2f GB", vp.memoryAlloc))
+                }
+                
+                Slider(value: $vp.memoryAlloc,
+                       in: (Double(VM.minMem)+0.5)...Double(VM.maxMem),
+                       step: 0.5)
+                    .padding(.horizontal, 10)
+                    .disabled(vp.autoMem)
+            }
+            .disabled(started)
+            .padding()
+            .alert(isPresented: $errorShown, content: {
+                Alert(title: Text(errorMessage))
+            })
+            
+            Divider()
+            
+            HStack {
+                Button("Connect") {
+                    VM.connect()
+                }
+                .disabled(!started)
+                
+                Spacer()
+                
+                Toggle(isOn: $started) {
+                    Text("VM")
+                }
+                .onChange(of: started, perform: { running in
+                    if running {
+                        startVM()
+                    } else {
+                        VM.stop()
+                    }
+                })
+                .toggleStyle(SwitchToggleStyle())
+            }
+            .padding()
+        }
+        .padding()
+        .frame(width: 500, height: 550, alignment: .center)
+        .onAppear {
+            loadData()
+        }
+        
+    }
+    
+    func saveDefaults() {
+        UserDefaults.standard.set(vp.autoCore, forKey: "autoCore")
+        UserDefaults.standard.set(vp.autoMem, forKey: "autoMem")
+        UserDefaults.standard.set(vp.memoryAlloc, forKey: "memAlloc")
+        UserDefaults.standard.set(vp.coreAlloc, forKey: "coreAlloc")
+        UserDefaults.standard.set(vp.ramdiskPath, forKey: "ramdiskPath")
+        UserDefaults.standard.set(vp.kernelPath, forKey: "kernelPath")
+        UserDefaults.standard.set(vp.diskPath, forKey: "diskPath")
+    }
+    
+    func startVM() {
+        guard vp.kernelPath != "" else {
+            started = false
+            errorMessage = "Missing kernel path."
+            errorShown = true
+            return
+        }
+        guard vp.ramdiskPath != "" else {
+            started = false
+            errorMessage = "Missing ramdisk path."
+            errorShown = true
+            return
+        }
+        guard vp.diskPath != "" else {
+            started = false
+            errorMessage = "Missing disk path."
+            errorShown = true
+            return
+        }
+        
+        saveDefaults()
+        
+        do {
+            try VM.configure(vp: self.vp)
+            try VM.start()
+        } catch {
+            started.toggle()
+        }
+    }
+    
+    func openFile(kind: String) -> String {
+        let dialog = NSOpenPanel()
+        dialog.title = "Select your \(kind)"
+        dialog.allowsMultipleSelection = false
+        dialog.canChooseDirectories = false
+        dialog.showsResizeIndicator = true
+        
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            if let url = dialog.url {
+                return url.path
+            }
+        }
+        
+        return ""
+    }
+}
+
+class VMParameters: NSObject, ObservableObject {
+    @Published var kernelParams = "console=hvc0"
+    @Published var kernelPath = ""
+    @Published var ramdiskPath = ""
+    @Published var diskPath = ""
+    // in GB - very lazy
+    @Published var memoryAlloc: Double = Double(VZVirtualMachineConfiguration.minimumAllowedMemorySize/(1024*1024*1024)) + 0.5
+    @Published var autoCore = true
+    @Published var autoMem = true
+    @Published var coreAlloc: Double = Double(VZVirtualMachineConfiguration.minimumAllowedCPUCount)
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ VFHost/VirtualMachine.swift
@@ -0,0 +1,149 @@
+//
+//  VirtualMachine.swift
+//  VFHost
+//
+//  Created by Jack Steele on 2/4/21.
+//
+
+import Foundation
+import Cocoa
+import Darwin
+import Virtualization
+import os.log
+
+class VirtualMachine: ObservableObject {
+    var cfg: VZVirtualMachineConfiguration?
+    var vm: VZVirtualMachine?
+    
+    let coreRange = Double(VZVirtualMachineConfiguration.maximumAllowedCPUCount - VZVirtualMachineConfiguration.minimumAllowedCPUCount)
+    let maxCore = Double(VZVirtualMachineConfiguration.maximumAllowedCPUCount)
+    let minCore = Double(VZVirtualMachineConfiguration.minimumAllowedCPUCount)
+    
+    // in GB
+    let memRange = (VZVirtualMachineConfiguration.maximumAllowedMemorySize - VZVirtualMachineConfiguration.minimumAllowedMemorySize)/(1024*1024*1024)
+    let maxMem = VZVirtualMachineConfiguration.maximumAllowedMemorySize/(1024*1024*1024)
+    let minMem = VZVirtualMachineConfiguration.minimumAllowedMemorySize/(1024*1024*1024)
+    
+    var ptyFD: Int32 = 0
+    var ptyPath = ""
+    
+    @Published var running = false
+    
+    func configure(vp: VMParameters) throws {
+        let config = VZVirtualMachineConfiguration()
+        
+        let bootLoader = VZLinuxBootLoader(kernelURL: URL(string: "file://\(vp.kernelPath)")!)
+        if vp.ramdiskPath != "" {
+            bootLoader.initialRamdiskURL = URL(string: "file://\(vp.ramdiskPath)")!
+        }
+        bootLoader.commandLine = vp.kernelParams
+        
+        config.bootLoader = bootLoader
+        
+        do {
+            let storage = try VZDiskImageStorageDeviceAttachment(url: URL(string: "file://\(vp.diskPath)")!, readOnly: false)
+            let blockDevice = VZVirtioBlockDeviceConfiguration(attachment: storage)
+            config.storageDevices = [blockDevice]
+        } catch {
+            os_log("Couldn't attach disk image")
+            return
+        }
+        
+        let ptyFD = configurePTY()
+        if ptyFD == -1 {
+            // should throw something more descriptive
+            throw VZError(.internalError)
+        }
+        
+        let mfile = FileHandle.init(fileDescriptor: ptyFD)
+        let console = VZVirtioConsoleDeviceSerialPortConfiguration()
+        console.attachment = VZFileHandleSerialPortAttachment(fileHandleForReading: mfile, fileHandleForWriting: mfile)
+        config.serialPorts = [console]
+        
+        
+        let balloonConfig = VZVirtioTraditionalMemoryBalloonDeviceConfiguration()
+        config.memoryBalloonDevices = [balloonConfig]
+        
+        let entropyConfig = VZVirtioEntropyDeviceConfiguration()
+        config.entropyDevices = [entropyConfig]
+        
+        let networkConfig = VZVirtioNetworkDeviceConfiguration()
+        networkConfig.attachment = VZNATNetworkDeviceAttachment()
+        config.networkDevices = [networkConfig]
+                
+        if !vp.autoCore {
+            config.cpuCount = Int(vp.coreAlloc)
+        }
+        
+        if !vp.autoMem {
+            config.memorySize = UInt64(vp.memoryAlloc * 1024*1024*1024)
+        } else {
+            var mem = (memRange / 4) + minMem
+            mem = mem * 1024*1024*1024
+            config.memorySize = mem
+        }
+        
+        try config.validate()
+        
+        os_log(.error, "VM configuration validation succeeded")
+        cfg = config
+    }
+    
+    func configurePTY() -> Int32 {
+        var ptyFD: Int32 = 0
+        var sfd: Int32 = 1
+
+        if openpty(&ptyFD, &sfd, nil, nil, nil) == -1 {
+            os_log(.error, "Error opening PTY")
+            return -1
+        }
+        self.ptyPath = String(cString: ptsname(ptyFD))
+        self.ptyFD = ptyFD
+        
+        return ptyFD
+    }
+    
+    func start() throws {
+        vm = VZVirtualMachine(configuration: cfg!)
+        vm?.start { result in
+            switch result {
+            case .success():
+                os_log("VM started")
+            case .failure(_):
+                os_log(.error, "Error starting VM")
+            }
+        }
+    }
+    
+    func stop() {
+        if vm != nil {
+            vm = nil
+            // lol
+            os.close(ptyFD)
+            os_log("VM stopped")
+        }
+    }
+    
+    func isRunning() -> Bool {
+        if vm?.state == .running {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func status() -> VZVirtualMachine.State? {
+        return vm?.state
+    }
+    
+    func connect() {
+        let script = "tell application \"Terminal\" to do script \"screen \(ptyPath)\""
+        let applescript = NSAppleScript(source: script)
+        var error: NSDictionary?
+        applescript?.executeAndReturnError(&error)
+        if let error = error {
+            NSLog(error["NSAppleScriptErrorMessage"] as! String)
+        }
+        
+    }
+}
