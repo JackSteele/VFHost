93b61a6760b931a94717bc406722f976d550843d
Jack Steele <jacks4899@gmail.com>
2021-02-08 23:43:02 -0800

    Prevent multiple tabs/windows

--- VFHost/VFHostApp.swift
+++ VFHost/VFHostApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct VFHostApp: App {
+
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .commands {
+            CommandGroup(replacing: CommandGroupPlacement.newItem, addition: { })
+        }
+    }
+    
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        NSWindow.allowsAutomaticWindowTabbing = false
     }
 }
