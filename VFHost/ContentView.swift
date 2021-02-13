0d78241b16cf51b0d118620f676324ae770afc32
Jack Steele <jacks4899@gmail.com>
2021-02-12 16:17:11 -0800

    Make main window hide instead of quit app Add confirmation on VM stop

--- VFHost/ContentView.swift
+++ VFHost/ContentView.swift
@@ -11,25 +11,27 @@ import Virtualization
 struct ContentView: View {
     @ObservedObject var VM = VirtualMachine()
     @ObservedObject var MM = ManagedMode()
-
+    
     let paramLimits = ParameterLimits()
     @EnvironmentObject var appDelegate: AppDelegate
     @State var windowDelegate = WindowDelegate()
-
+    
     @State private var window: NSWindow?
     @State var installProgress = 0.0
     @State var errorShown = false
+    @State var killConfirmationShown = false
+    @State var killCancelled = false
     @State var uninstallConfirmationShown = false
     @State var errorMessage = ""
     @State var started = false
     @State var managed = true
     @State var height = 300
     @State var installed: [Distro?] = []
-
+    
     @StateObject var params = UIParameters()
-
+    
     let timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
-
+    
     func loadData() {
         if let def = UserDefaults.standard.string(forKey: "kernelPath") {
             params.kernelPath = def
@@ -50,20 +52,28 @@ struct ContentView: View {
             self.managed = true
         }
     }
-
+    
     var body: some View {
         VStack {
             HStack {
                 Spacer()
-                Text(started ? "VM running" : "VM stopped").font(.largeTitle)
+                if killConfirmationShown {
+                    Text("VM stopping").font(.largeTitle)
+                } else {
+                    Text(started ? "VM running" : "VM stopped").font(.largeTitle)
+                }
                 Spacer()
-
+                
                 Toggle(isOn: $started) {
                     Text("")
                 }
                 .onChange(of: started, perform: { running in
-                    if managed {
-                        if running {
+                    if killCancelled {
+                        killCancelled = false
+                        return
+                    }
+                    if running {
+                        if managed {
                             appDelegate.canTerminate = false
                             windowDelegate.canTerminate = false
                             if let distro = MM.installed.first {
@@ -72,29 +82,26 @@ struct ContentView: View {
                                 MM.vm.attachScreen()
                             }
                         } else {
-                            appDelegate.canTerminate = true
-                            windowDelegate.canTerminate = true
-                            MM.stopVM()
-                        }
-                    } else {
-                        if running {
                             appDelegate.canTerminate = false
                             windowDelegate.canTerminate = false
                             if validateParams() {
                                 saveDefaults()
                                 startVM()
                             }
-                        } else {
-                            appDelegate.canTerminate = true
-                            windowDelegate.canTerminate = true
-                            VM.stop()
                         }
+                    } else {
+                        self.killConfirmationShown.toggle()
                     }
                 })
                 .toggleStyle(SwitchToggleStyle())
                 .disabled(managed ? (MM.installed.count == 0) : false)
-                .alert(isPresented: $errorShown, content: {
-                    Alert(title: Text(errorMessage))
+                .alert(isPresented: $killConfirmationShown, content: {
+                    Alert(title: Text("Really stop this VM?"), primaryButton: .default(Text("Keep running"), action: {
+                        self.killCancelled = true
+                        self.started = true
+                    }), secondaryButton: .destructive(Text("Stop VM"), action: {
+                        kill()
+                    }))
                 })
             }
             Divider()
@@ -119,7 +126,6 @@ struct ContentView: View {
                             Alert(title: Text("Uninstall?"),
                                   message: Text("Are you sure you'd like to uninstall this VM?"),
                                   primaryButton: .cancel(Text("Don't do it!"), action: {
-                                    //                    self.uninstallConfirmationShown = false
                                   }), secondaryButton: .destructive(Text("Uninstall"), action: {
                                     for distro in MM.installed {
                                         MM.rmDistro(distro!)
@@ -143,7 +149,7 @@ struct ContentView: View {
                         }
                         .disabled(MM.installing)
                         .padding()
-
+                        
                         if MM.installing {
                             ProgressView(value: installProgress)
                                 .padding()
@@ -170,7 +176,7 @@ struct ContentView: View {
                             params.kernelPath = openFile(kind: "kernel")
                         }
                     }
-
+                    
                     Text("Ramdisk path")
                     HStack {
                         TextField("~/distribution/initrd", text: $params.ramdiskPath)
@@ -178,7 +184,7 @@ struct ContentView: View {
                             params.ramdiskPath = openFile(kind: "ramdisk")
                         }
                     }
-
+                    
                     Text("Disk image path")
                     HStack {
                         TextField("~/distribution/disk.img", text: $params.diskPath)
@@ -186,15 +192,18 @@ struct ContentView: View {
                             params.diskPath = openFile(kind: "disk image")
                         }
                     }
-
+                    
                     Text("Kernel parameters")
                     HStack {
                         TextField("console=hvc0", text: $params.kernelParams)
                     }
                 }
+                .alert(isPresented: $errorShown, content: {
+                    Alert(title: Text(errorMessage))
+                })
                 .disabled(started)
                 .padding()
-
+                
                 Form {
                     HStack {
                         Text("CPU cores allocated")
@@ -204,14 +213,14 @@ struct ContentView: View {
                         Spacer()
                         Text(params.autoCore ? "" : "\(Int(params.coreAlloc)) core(s)")
                     }
-
+                    
                     Slider(value: $params.coreAlloc,
                            in: paramLimits.minCores...paramLimits.maxCores,
                            step: 1
                     )
                     .padding(.horizontal, 10)
                     .disabled(params.autoCore)
-
+                    
                     HStack {
                         Text("Memory allocated")
                         Toggle(isOn: $params.autoMem, label: {
@@ -220,7 +229,7 @@ struct ContentView: View {
                         Spacer()
                         Text(params.autoMem ? "" : String(format: "%.2f GB", params.memoryAlloc))
                     }
-
+                    
                     Slider(value: $params.memoryAlloc,
                            in: paramLimits.minMem...paramLimits.maxMem,
                            step: 0.5)
@@ -230,10 +239,10 @@ struct ContentView: View {
                 .disabled(started)
                 .padding()
             }
-
+            
             /// Bottom bit
             Divider()
-
+            
             HStack {
                 Button("Reconnect") {
                     if managed {
@@ -272,14 +281,30 @@ struct ContentView: View {
         //                  }))
         //        })
     }
-
+    
+    func kill() {
+        if managed {
+            appDelegate.canTerminate = true
+            windowDelegate.canTerminate = true
+            MM.stopVM()
+        } else {
+            appDelegate.canTerminate = true
+            windowDelegate.canTerminate = true
+            VM.stop()
+        }
+    }
+    
+    func killUnmanaged() {
+        
+    }
+    
     func saveDefaults() {
         UserDefaults.standard.set(params.ramdiskPath, forKey: "ramdiskPath")
         UserDefaults.standard.set(params.kernelPath, forKey: "kernelPath")
         UserDefaults.standard.set(params.diskPath, forKey: "diskPath")
         UserDefaults.standard.set(params.kernelParams, forKey: "kernelParams")
     }
-
+    
     func validateParams() -> Bool {
         guard params.kernelPath != "" else {
             started = false
@@ -301,7 +326,7 @@ struct ContentView: View {
         }
         return true
     }
-
+    
     func startVM() {
         do {
             let vp = VMParameters(kernelParams: params.kernelParams, kernelPath: params.kernelPath, ramdiskPath: params.ramdiskPath, diskPath: params.diskPath, memoryAlloc: params.memoryAlloc, autoCore: params.autoCore, autoMem: params.autoMem, coreAlloc: params.coreAlloc)
@@ -312,7 +337,7 @@ struct ContentView: View {
             started.toggle()
         }
     }
-
+    
     func termPerms() -> Bool {
         let script = "tell application \"Terminal\" to activate"
         let applescript = NSAppleScript(source: script)
@@ -323,14 +348,14 @@ struct ContentView: View {
         }
         return true
     }
-
+    
     func openFile(kind: String) -> String {
         let dialog = NSOpenPanel()
         dialog.title = "Select your \(kind)"
         dialog.allowsMultipleSelection = false
         dialog.canChooseDirectories = false
         dialog.showsResizeIndicator = true
-
+        
         if dialog.runModal() == NSApplication.ModalResponse.OK {
             if let url = dialog.url {
                 return url.path
@@ -343,7 +368,7 @@ struct ContentView: View {
 struct WindowAccessor: NSViewRepresentable {
     @Binding var window: NSWindow?
     @Binding var windowDelegate: WindowDelegate
-
+    
     func makeNSView(context: Context) -> some NSView {
         let view = NSView()
         DispatchQueue.main.async {
@@ -352,36 +377,39 @@ struct WindowAccessor: NSViewRepresentable {
         }
         return view
     }
-
+    
     func updateNSView(_ nsView: NSViewType, context: Context) {
-
+        
     }
 }
 
 class WindowDelegate: NSObject, NSWindowDelegate {
     var canTerminate = true
-
+    
     func windowShouldClose(_ sender: NSWindow) -> Bool {
-        if canTerminate {
-            return true
-        } else {
-            let alert = NSAlert()
-            alert.messageText = "Really quit?"
-            alert.informativeText = "You're about to close VFHost with a VM running. Is this what you want?"
-            alert.addButton(withTitle: "No, don't quit.")
-            alert.addButton(withTitle: "Yes, quit.")
-            alert.alertStyle = .critical
-            let res = alert.runModal()
-            if res == .alertFirstButtonReturn {
-                return false
-            } else if res == .alertSecondButtonReturn {
-                // I feel bad for this
-                // sincerely
-                exit(0)
-//                return true
-            }
-            return true
-        }
+        NSApplication.shared.hide(sender)
+        return false
+        
+        //        if canTerminate {
+        //            return true
+        //        } else {
+        //            let alert = NSAlert()
+        //            alert.messageText = "Really quit?"
+        //            alert.informativeText = "You're about to close VFHost with a VM running. Is this what you want?"
+        //            alert.addButton(withTitle: "No, don't quit.")
+        //            alert.addButton(withTitle: "Yes, quit.")
+        //            alert.alertStyle = .critical
+        //            let res = alert.runModal()
+        //            if res == .alertFirstButtonReturn {
+        //                return false
+        //            } else if res == .alertSecondButtonReturn {
+        //                // I feel bad for this
+        //                // sincerely
+        //                exit(0)
+        ////                return true
+        //            }
+        //            return true
+        //        }
     }
 }
 
